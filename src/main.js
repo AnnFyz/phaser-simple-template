1ef1286bd56d4c3a4c19ef725bf1361bf8f38678
Anna <annfyz1232@yandex.ru>
2023-10-21 14:41:40 +0200

    add

---
+++ src/main.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene: [MainScene],
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        }
+    };
+
+    const game = new Phaser.Game(config);
+}
