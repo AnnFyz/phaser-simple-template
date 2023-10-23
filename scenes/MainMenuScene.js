e71a0cc8c8c2f7d017c7faa43b21564ab41fe0c0
Anna <annfyz1232@yandex.ru>
2023-10-23 21:40:34 +0200

    add

--- scenes/MainMenuScene.js
+++ scenes/MainMenuScene.js
@@ -4,21 +4,42 @@ class MainMenuScene extends Phaser.Scene {
     }
 
     preload() {
-        scene.load.scenePlugin('rexuiplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js', 'rexUI', 'rexUI');
+        this.load.scenePlugin({
+            key: 'rexuiplugin',
+            url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
+            sceneKey: 'rexUI'
+        });
         this.load.image('button', 'assets/UI_Panel_Background_5-04.png');
         this.button = {};
+
     }
     create() {
         this.cameras.main.fadeIn(2000);
         console.log("mainMenu");
         var text = this.add.text(game.config.width / 2, game.config.height / 2, "main menu", { font: "32px Arial", fill: "#FFFFFF", align: "center" });
         text.setOrigin(.5, .5);
-        this.createButton(this);
+        // this.createButton(this);
+       
     }
 
-    createButton(scene) {
+    /*createButton(scene) {
         scene.button = scene.add.button(game.config.width / 2, game.config.height / 2, 'button', scene.actionOnClick, scene, 2, 1, 0);
         console.log("Button was created");
+    }
+*/
+    createButtons(scene) {
+        scene.buttons = scene.rexUI.add.buttons({
+            x: 400, y: 300,
+            orientation: 'y',
+
+            buttons: [
+                createButton(this, 'A'),
+                createButton(this, 'B'),
+            ],
+
+            space: { item: 8 }
+
+        })
     }
 
     actionOnClick() {
